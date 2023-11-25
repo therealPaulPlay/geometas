dd559e4136582db2698b8805265d12ccaca94df3
F1Laps <contact@f1laps.com>
2023-11-25 20:21:51 +0100

    Improvements

--- geobin/quiz/views.py
+++ geobin/quiz/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.conf import settings
 
-from quiz.airtable_api import get_question
+from quiz.airtable_api import get_random_fact, get_specific_fact
 
-def quiz(request):
-    fact = get_question()
+
+def quiz(request, fact_id=None):
+    if fact_id:
+        fact = get_specific_fact(fact_id)
+    else:
+        fact = get_random_fact()
     context = {'fact': fact}
     return render(request, 'quiz/question.html', context)
+
+
+# Fact with formatted notes: rec3BZ4znZPlTNyS0
