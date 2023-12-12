b134b2c0a98427df7570d12d7a611fd5f76c9922
F1Laps <contact@f1laps.com>
2023-12-12 20:28:23 +0100

    Working quiz

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:fact_uuid>/q/', views.question, name='question'),
-    path('<slug:fact_uuid>/a/', views.answer, name='answer'),
-    path('', views.quiz, name='quiz'),
+    path('<slug:quiz_uuid>/<slug:fact_uuid>/q/', views.question, name='question'),
+    path('<slug:quiz_uuid>/<slug:fact_uuid>/a/', views.answer, name='answer'),
+    path('<slug:quiz_uuid>/', views.quiz, name='quiz'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -4,22 +4,32 @@ import random
 from .models import Fact, Quiz
 
 
-def quiz(request, fact_id=None):
-    # Get a random Fact uuid from quiz.models.Fact
+def quiz(request, quiz_uuid):
+    quiz = Quiz.objects.get(uuid=quiz_uuid)
     facts = Fact.objects.all()
+    if quiz.category:
+        facts = facts.filter(category=quiz.category)
+    if quiz.countries and quiz.countries.count() > 0:
+        facts = facts.filter(countries__in=quiz.countries.all())
     random_fact_id = random.choice(facts).uuid
-    return redirect('quiz:question', fact_uuid=random_fact_id)
+    return redirect('quiz:question', quiz_uuid=quiz_uuid, fact_uuid=random_fact_id)
 
 
-def question(request, fact_uuid):
+def question(request, quiz_uuid, fact_uuid):
     fact = Fact.objects.get(uuid=fact_uuid)
-    context = {'fact': fact}
+    context = {
+        'fact': fact,
+        'quiz_uuid': quiz_uuid
+    }
     return render(request, 'quiz/question.html', context)
 
 
-def answer(request, fact_uuid):
+def answer(request, quiz_uuid, fact_uuid):
     fact = Fact.objects.get(uuid=fact_uuid)
-    context = {'fact': fact}
+    context = {
+        'fact': fact,
+        'quiz_uuid': quiz_uuid
+    }
     return render(request, 'quiz/answer.html', context)
 
 
